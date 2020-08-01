9d478d6d6a316552282085de5e24ea3efba73734
SabiKov <skovacs33@gmail.com>
2020-08-02 00:19:15 +0200

    added init spring

---
+++ hometracking/src/main/java/com/sabi11/hometracking/HometrackingApplication.java
@@ -0,0 +1,13 @@
+package com.sabi11.hometracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometrackingApplication.class, args);
+	}
+
+}
---
+++ hometracking/src/test/java/com/sabi11/hometracking/HometrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabi11.hometracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
